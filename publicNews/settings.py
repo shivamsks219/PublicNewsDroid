a97fa85a285dcfa46c39080aa6e3b355a5517e4f
Shivam Kumar <68140446+shivamsks219@users.noreply.github.com>
2023-05-23 17:37:51 +0530

    add vercel

--- publicNews/settings.py
+++ publicNews/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--38#2re@g2(xq-i2a#0o=8hi8nhn88q92mafbd0de4#=k(1ej1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
